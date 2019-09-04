3a76e5606434d9aed66d971e3abab367f14c9719
Rioditylo <00083917@uca.edu.sv>
2019-09-03 20:19:25 -0600

    agregar tarea

---
+++ ejercicios de labo/ejercicio 19.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+double **crear_matriz(int n,int m){
+	ptr = new double*[n];
+	
+	for(int i=0; i<n; i++)
+	ptr[i] = new double[m];
+	
+	return ptr;
+}
+int main(){
+	
+	int n, int m;
+	ptr =  crear_ matriz
+n=5
+m=5
+
+double **ptr
+ptr= new double*[n];
+
+for(int i=0; i<n, i++)
+for(int j=0; j<m; j++)
+cin>> ptr[i][j;
+
+for (int i=0; i<n; i++ ){
+	for (int j=0; j<m;j++)
+	cout<<ptr[i][j]<<"";
+	cout<<endl;
+}
+}
